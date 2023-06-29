30af4520f3ca5a920aa5cbb56ec8413bb45ed8ed
Nikita Glazov <mikromolekula110@yandex.ru>
2023-06-29 21:08:38 +0300

    added header and ingredients

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from './app-header.module.css';
+import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+
+
+function AppHeader () {
+  return (
+      <header className={styles.header}>
+        <nav className={styles.menu}>
+          <div className={`pt-4 pr-5 pb-4 pl-5 ${styles.menu_item}`}>
+            <BurgerIcon type="primary" />
+            <p className="text text_type_main-small">Конструктор</p>
+          </div>
+          <div className={`pt-4 pr-5 pb-4 pl-5 ${styles.menu_item}`}>
+            <ListIcon type="secondary" />
+            <p className="text text_type_main-small text_color_inactive">Лента заказов</p>
+          </div>
+        </nav>
+        <div className={styles.logo}>
+          <Logo />
+        </div>
+        <div>
+          <div className={`pt-4 pr-5 pb-4 pl-4 ${styles.menu_item}`}>
+            <ProfileIcon type="secondary" />
+            <p className="text text_type_main-small text_color_inactive">Личный кабинет</p>
+          </div>
+        </div>
+      </header>
+    )
+}
+
+export default AppHeader;
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,23 @@
 import styles from "./app.module.css";
 import { data } from "../../utils/data";
+import AppHeader from "../app-header/app-header.jsx";
+import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 
 function App() {
   return (
+    
     <div className={styles.app}>
-      <pre style={{
-      	margin: "auto",
-      	fontSize: "1.5rem"
-      }}>
+      <AppHeader />
+        <main>
+        { data.length > 0 &&
+              <>
+                <BurgerIngredients data={data} />
+                
+              </>
+          }
+        </main>
       	
-      </pre>
+ 
     </div>
   );
 }
