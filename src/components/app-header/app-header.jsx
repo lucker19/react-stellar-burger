e79428bb4b864be32e5e707e8fbf264aeadd5e2b
Nikita Glazov <mikromolekula110@yandex.ru>
2023-07-02 23:27:25 +0300

    fixed links

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -7,23 +7,23 @@ function AppHeader () {
   return (
       <header className={styles.header}>
         <nav className={styles.menu}>
-          <div className={`pt-4 pr-5 pb-4 pl-5 ${styles.menu_item}`}>
+          <a className={`pt-4 pr-5 pb-4 pl-5 ${styles.menu_item}`}>
             <BurgerIcon type="primary" />
             <p className="text text_type_main-small">Конструктор</p>
-          </div>
-          <div className={`pt-4 pr-5 pb-4 pl-5 ${styles.menu_item}`}>
+          </a>
+          <a className={`pt-4 pr-5 pb-4 pl-5 ${styles.menu_item}`}>
             <ListIcon type="secondary" />
             <p className="text text_type_main-small text_color_inactive">Лента заказов</p>
-          </div>
+          </a>
         </nav>
         <div className={styles.logo}>
           <Logo />
         </div>
         <div>
-          <div className={`pt-4 pr-5 pb-4 pl-4 ${styles.menu_item}`}>
+          <a className={`pt-4 pr-5 pb-4 pl-4 ${styles.menu_item}`}>
             <ProfileIcon type="secondary" />
             <p className="text text_type_main-small text_color_inactive">Личный кабинет</p>
-          </div>
+          </a>
         </div>
       </header>
     )
